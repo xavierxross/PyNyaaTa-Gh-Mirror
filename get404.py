cad684e2093a89044db8eeb5b2cf963350776371
Xéfir Destiny <xefir@crystalyx.net>
2019-12-11 19:40:35 +0100

    Add get404 cron

---
+++ get404.py
@@ -0,0 +1,13 @@
+from config import app
+from connectors import Connector
+from models import AnimeLink
+
+app.config['SQLALCHEMY_ECHO'] = False
+links = AnimeLink.query.all()
+
+for link in links:
+    connect = Connector.get_instance(link.link, link.title.keyword)
+    html = connect.curl_content(link.link)
+
+    if html['http_code'] is not 200:
+        print('(%d) %s %s : %s' % (html['http_code'], link.title.name, link.season, link.link))
